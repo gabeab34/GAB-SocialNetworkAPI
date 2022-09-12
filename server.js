fe4a205fe03f5c2fd49e251be4d795762dfe0777
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-09-12 14:42:39 -0400

    added main server file

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import db from "./config/connection"
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json());
+app.use(routes);
+
+db.once("open", () => {
+    app.listen(PORT, () => {
+        console.log(`Server now running on port ${PORT}!`);
+    })
+})
